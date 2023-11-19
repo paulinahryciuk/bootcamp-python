400a9d5e976ac6d81ef3ee92a128ccf5386cde39
Paulina <hryciuk.paulina1@gmail.com>
2023-11-19 12:10:58 +0100

    update commit

--- 2.lista.py
+++ 2.lista.py
@@ -23,6 +23,78 @@ print(lista[1])
 print(lista[-1])
 print(lista[-2])
 
+print(lista[0:1])
+print(lista[:1])
+print(lista[-2:])
+print(lista[:])
+print(lista[-1:0])
+
+lista[1] = "CCC"
+print(lista)
+lista.insert(1,"YYY")
+print(lista)
+
+print(lista.pop(1))
+print(lista)
+ind = lista.index("A")
+print(ind)
+print(lista.pop(ind))
+print(lista)
+
+print(lista.remove("CCC"))
+print(lista)
+
+lista.append('ola')
+lista.append('OLa')
+lista.append('ania')
+print("Ola" in lista)
+print("ola" in lista)
+print(lista)
+
+liczby = [45, 999, 34, 22, 13.34,687]
+print(liczby)
+print(type(liczby))
+liczby.sort()
+print(liczby)
+
+lista_osoby = [ 'radek', 'ola', 'agata', 'lena']
+print(lista_osoby)
+liczby = [45, 999, 34, 22, 13.34,687, 'A']
+
+lista_osoby.sort(reverse= True)
+print(lista_osoby)
+print("#")
+print(liczby)
+elem = liczby[3]
+liczby [3] = 10000
+print(liczby)
+print(liczby[-1])
+print(liczby[(len(liczby))-1])
+print(liczby)
+liczby.pop(0)
+liczby.remove(10000)
+print("---")
+print("usuniecie",liczby)
+print(liczby[0:])
+print(liczby[-1:0])
+print("---")
+
+print(liczby + lista)
+liczby6 = liczby + lista
+print(liczby6)
+liczby = [1,2,3,4]
+liczby2 = [5,6,7]
+liczby2.extend(liczby)
+print(liczby2)
+
+tekst = 'Python'
+lista_str = list(tekst)
+print(lista_str)
+
+krotka = tuple(liczby)
+print(krotka)
+
+
 
 
 

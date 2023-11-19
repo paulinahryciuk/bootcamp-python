cd8daeaee790ec06fdaaca479d7f247506cfd52e
Paulina <hryciuk.paulina1@gmail.com>
2023-11-19 09:54:02 +0100

    init commit

---
+++ 2.4 typu danych.py
@@ -0,0 +1,47 @@
+wiek = 47
+rok = 2023
+temp = 36.6
+print(temp)
+print(type(temp))
+
+print(3//2)
+print(wiek%rok)
+print(wiek**rok)
+
+print(74 - 8 * 45 +8/2+8/2)
+print(74 - (8 * 45) +(8/2)+(8/2))
+
+print(f"{wiek ** rok:,}")
+
+print(0.2+0.7)
+wynik = 0.2+0.7
+print(f"{wynik:.1f}")
+print(f"sprawdzenie {wiek}")
+print(f"""
+{wiek}""")
+
+czy_znaz_py = True
+print(czy_znaz_py)
+print(type(czy_znaz_py))
+print(int(czy_znaz_py))
+
+x = 1
+print(bool(x))
+
+a = 14
+b = 3
+print(f"wynik porowanian {a} > {b} = {a>b}")
+
+print(True or True)
+print(not True)
+
+my_str = '123456789'
+print(my_str.isalnum())
+print("#")
+str = "10.1"
+print(str.isalnum())
+print(str.isalpha())
+print(str.isdigit())
+print(str.isdecimal())
+print((str.isascii()))
+print((str.isprintable()))

e8978ab0cbc0c253cfbd70458104f9a14e82ca27
Paulina <hryciuk.paulina1@gmail.com>
2023-11-19 10:14:12 +0100

    update commit

---
+++ 2.lista.py
@@ -0,0 +1,36 @@
+lista = []
+print((lista))
+print(type(lista))
+print(bool(lista))
+
+lista2 = [10,20,30]
+print(lista2)
+
+lista5 = [10.77, 30.66]
+print(lista5)
+
+lista4 = [10, 5.2, "oko"]
+print(lista4)
+
+print(len(lista4))
+
+
+lista.append("A")
+lista.append("B")
+print(lista)
+print(lista[0],lista[-2])
+print(lista[1])
+print(lista[-1])
+print(lista[-2])
+
+
+
+
+
+
+
+
+
+
+
+

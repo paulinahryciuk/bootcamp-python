ca3d013616354ab1b447aa0d764d4b1bd2115150
Paulina <hryciuk.paulina1@gmail.com>
2023-11-19 08:42:01 +0100

    init commit

---
+++ 1.1 - pierwszy.py
@@ -0,0 +1,20 @@
+# name = "paulina"
+# name2 = name.lower()
+# name3 = name.capitalize()
+# print(name2)
+# print(name3)
+# print(name.count("a",0,2))
+# print("moj ulubiony serial\"Alternatywy\"")
+# for_tekst = f"mam na imie{name}"
+# print(for_tekst)
+
+# name = "ola"
+# age = 10
+# print(f"hej {name} masz {age} lat")
+# print("witaj %(name)")
+# print("uzuwa %i")
+# wersja = 3.98
+# print(f"uzyw{wersja:.0f}")
+liczba = 123456789
+liczba2 = liczba:,
+print(liczba2)

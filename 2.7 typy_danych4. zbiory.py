6dc6a6bf77106cb5402085f19b3c96dfea74f2f5
Paulina <hryciuk.paulina1@gmail.com>
2023-11-19 15:03:32 +0100

    update

---
+++ 2.7 typy_danych4. zbiory.py
@@ -0,0 +1,40 @@
+lista = [44, 55, 66, 777, 33, 11, 33, 11]
+zbior = set(lista)
+print(set)
+lista2 = list(zbior)
+print(lista2)
+lista2.remove((11))
+print((lista2))
+
+zb2 = set()
+print(zb2)
+print(type(zb2))
+zb2.add(2)
+zb2.add(3)
+zb2.add(5)
+zb2.add(5)
+zb2.add(3)
+print((zb2))
+print(zb2.pop())
+print(zb2)
+
+zb2.remove(5)
+print(zb2)
+
+zbior2 = {667, 11, 44, 18, 52, 22, 667, 62, 999}
+print(zbior2)
+
+print(zbior | zbior2)
+print(zbior.union(zbior2))
+zbior3 = {8, 9, 10}
+print(zbior.union(zbior2,zbior3))
+print((zbior & zbior2))
+print(zbior-zbior2)
+print(zbior.difference(zbior2))
+
+zbior4 = zbior2.copy()
+print((zbior4))
+zbior2.clear()
+print((zbior4))
+print((zbior2))
+zbior2.update(zbior4)

9e00fd09896f07c64e0c8a8440448b9610f9da6e
Paulina <hryciuk.paulina1@gmail.com>
2023-11-26 19:17:22 +0100

    homework

---
+++ 1 - 2 PD.py
@@ -0,0 +1,36 @@
+# 1
+fruits = ["jablko", "banan", "kiwi", "gruszka", "arbuz"]
+
+# 2
+print (fruits[1])
+
+# 3
+fruits [2] = "malina"
+
+# 4
+colors = ( "bialy", "czarny", "czerwony")
+
+# 5
+print(colors[-1])
+
+# 8
+me = "Paulina Hryciuk"
+other_string = "Jan Kowalski"
+
+# 9
+name_surname = other_string.split()
+name = name_surname[0]
+surname = name_surname[1]
+
+# 10
+print (f"Mam na imie {name} i mam 25 lat")
+
+# 1
+list = [1, 2, 3, 4, 5]
+print(len(list))
+
+# 2
+list_1 = [1,2,3]
+list_2 = [4,5,6]
+list_1.copy(list_2)
+print(list_1)

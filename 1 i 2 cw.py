59c5401a843956350e3f8c2ebab4a8fdfbeca493
Paulina <hryciuk.paulina1@gmail.com>
2023-11-26 18:05:39 +0100

    init

---
+++ 1 i 2 cw.py
@@ -0,0 +1,10 @@
+imie = "Ala"
+print (f"1 mam na imie {imie}")
+print ( "2 mam na imie {}".format(imie))
+# print( "3 mam na imie" % imie )
+
+licz = 2.56
+print ( f"zaokraglenei tej naszej liczby to {licz:.1f}")
+print ( f"a teraz {licz:.3}")
+liczba = 100000000000
+print (f"bilion {liczba:,}")

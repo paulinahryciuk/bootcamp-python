8876ee3969e2d0fc2e30a3371400d50cfa3a71cc
Paulina <hryciuk.paulina1@gmail.com>
2023-12-02 14:58:47 +0100

    update

---
+++ 3.5 petle_zad1.py
@@ -0,0 +1,27 @@
+# for i in range (10):
+#     print(i,i, sep =":", end = '')
+#
+# imie = "Paulina"
+# for i in imie:
+#     print(i)
+# import random
+# lista = list(range(1,50))
+# for n in range(1,6):
+#     a=random.choice(lista)
+#     lista.remove((a))
+#     print(a)
+#
+# lista = [5,10,23,36,8,16]
+# lista.sort(reverse=True)
+# print(lista)
+
+# lista = [n for n in range (1,10) if j%2==0]
+
+lista = ["ada", 'ola', 'ania']
+for i in range (len(lista)):
+    print(i, lista[i])
+
+ludzie = ["ada", 'ola', 'ania']
+wiek = [10,20,30]
+for n in range(len(ludzie)):
+    print(ludzie[n],wiek[n])

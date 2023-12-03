56e709615e656b0856d80a834fdfd6873a5521cc
Paulina <hryciuk.paulina1@gmail.com>
2023-12-03 09:23:23 +0100

    update

---
+++ 4.4 wyjatki zad2 kalkulator.py
@@ -0,0 +1,24 @@
+
+    dzialanie = input("Jakie dzialanie chcesz wykonac ? [+ - * / K- exit" )
+while dzialanie != k.lower:
+
+while True:
+        print(("""
+        1. dodawanie
+        5. koniec"""))
+
+odp = input("podaj dzialanie")
+if odp == "5":
+    break()
+try:
+    a = int("pierwsza liczba")
+    b = int("druga liczba")
+    if odp == "1":
+        print()
+    elif
+    else:
+        nie znam takiego
+except ZeroDivisionError:
+    print((nie dziel pzrez 0))
+except Exception as e:
+    print(Blad,e)

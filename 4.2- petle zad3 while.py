5bb3abb581b34adea449a286c419eb715d0e60d2
Paulina <hryciuk.paulina1@gmail.com>
2023-12-03 08:56:31 +0100

    update

---
+++ 4.2- petle zad3 while.py
@@ -0,0 +1,12 @@
+# while True:
+#     print("komunikat!!")
+#     licznik += >10:
+#     if licznik>10:
+#         break
+
+lista = []
+
+while True:
+    wej =input("podaj liczbe")
+    if not wej.isnumeric():
+        break

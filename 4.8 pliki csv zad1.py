0a4296d7cc13deb37c671aff73df91ba0390cbb5
Paulina <hryciuk.paulina1@gmail.com>
2023-12-03 11:15:27 +0100

    update

---
+++ 4.8 pliki csv zad1.py
@@ -0,0 +1,12 @@
+import csv
+
+fields = ['name','brench','tear']
+row = ['radek','cloe','5']
+
+zipped_dict = dict(zip(fields,row))
+print(zipped_dict)
+
+with open("records.csv", 'w', newline='') as csv_f:
+    csvwriter = csv.writer(csv_f)
+    csvwriter.writerow(fields)
+    csvwriter.writerow(row)

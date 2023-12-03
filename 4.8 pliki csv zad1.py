1477bcfd5aee42eee73ce0fa986155a778616882
Paulina <hryciuk.paulina1@gmail.com>
2023-12-03 11:18:09 +0100

    update

--- 4.8 pliki csv zad1.py
+++ 4.8 pliki csv zad1.py
@@ -7,6 +7,9 @@ zipped_dict = dict(zip(fields,row))
 print(zipped_dict)
 
 with open("records.csv", 'w', newline='') as csv_f:
-    csvwriter = csv.writer(csv_f)
-    csvwriter.writerow(fields)
-    csvwriter.writerow(row)
+    # csvwriter = csv.writer(csv_f)
+    # csvwriter.writerow(fields)
+    # csvwriter.writerow(row)
+    cswriter = csv.DictWriter(csv_f, fieldnames=field)
+    cswriter.writerow(zipped_dict)
+    cswriter.writerows(  )

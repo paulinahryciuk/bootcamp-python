9882696861f36a3a606410c4249d4fd93ab75dca
Paulina <hryciuk.paulina1@gmail.com>
2023-12-17 10:26:01 +0100

    update

--- 6.4 -kl2.py
+++ 6.4 -kl2.py
@@ -14,14 +14,27 @@ class Human:
         self.imie = imie
         self.wiek = wiek
         self.plec = plec
+        self.wzrost = wzrost
     def powitanie(self):
         print(f"nazywam sie {self.imie}")
 
     def wiekow(self):
         print(f"mam {self.wiek} lat")
+
+    def podaj_wzrost(self):
+        print(f"mam {self.wzrost}")
+
+    def ruszaj(self):
+        if self.plec =="m":
+            print("rusz-em")
+        else:
+            print("rusz-am")
+
+
 cz1 = Human("anna",27,170)
 print(cz1)
 print(cz1.imie)
 print(cz1.__doc__)
 print(cz1.__init__.__doc__)
-print(cz1.powitanie())
+cz1.powitanie()
+cz1.podaj_wzrost()

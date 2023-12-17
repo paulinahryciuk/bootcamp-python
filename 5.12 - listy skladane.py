5bdd533dbfd8ae1435a29e8f57ae6f42e6b327f9
Paulina <hryciuk.paulina1@gmail.com>
2023-12-17 08:19:53 +0100

    update

--- 5.12 - listy skladane.py
+++ 5.12 - listy skladane.py
@@ -1,3 +1,4 @@
 lista = [1,2,5]
 squared = [num ** 2 for num in lista]
 print(squared)
+`
---
+++ 6.0.py
@@ -0,0 +1,6 @@
+lista1 = [2,4,6,8,3]
+lista2 = [i**3 for i in lista1]
+print(lista2)
+
+lista3 = [i for i in lista1 if i != 8]
+print(lista3)
